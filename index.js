8e6c03ef3b13a9719c932db6a4e822f1dbef2df0
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-10-06 01:29:03 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ctrlCfg from "./model/ctrlCfg.js";
 import { pluginName, pluginDirName } from "./model/base.js";
 //
 let ret = [], apps = {};
-let pluginVersion = Object.keys(ctrlCfg.getDefSet("information", "updataLog"))[0];
+let pluginVersion = Object.keys(ctrlCfg.getDefSet("information", "updateLog"))[0];
 const appFiles = fs.readdirSync(`./plugins/${pluginDirName}/apps/`).filter(file => file.endsWith(".js"));
 //
 appFiles.forEach((file) => {
@@ -15,13 +15,13 @@ ret = await Promise.allSettled(ret);
 //启动插件输出日志
 let infoLog = [
 	"----------------------------",
-	`${pluginName}v${pluginVersion}插件开始初始化...`,
+	`${pluginName}(v${pluginVersion})插件开始初始化...`,
 ];
 //打印日志
 for (let log of infoLog) logger.info(log);
 //载入apps内JS插件
-for (let item in targetFiles) {
-	let name = targetFiles[item].replace(/(\.js)$/g, "");
+for (let item in appFiles) {
+	let name = appFiles[item].replace(/(\.js)$/g, "");
 	if (ret[item].status != "fulfilled") {
 		logger.error(`载入插件错误：${logger.red(name)}`);
 		logger.error(ret[i].reason);
@@ -32,4 +32,4 @@ for (let item in targetFiles) {
 //
 logger.info(`${pluginName}插件载入成功`);
 //
-export { pluginDirName , apps };
+export { pluginDirName, apps };
