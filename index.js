65ba2c3e341f2610368dc97791dff70743bc96b7
YooLan <61037787+Zacharvc@users.noreply.github.com>
2022-10-06 00:23:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import ctrlCfg from "./model/ctrlCfg.js";
+//
+let pluginVersion = Object.keys(ctrlCfg("information", "updataLog"))[0];
+let pluginDirName = "LoveStudy-plugin", pluginName = "学习", ret = [], apps = {};
+const appFiles = fs.readdirSync(`./plugins/${pluginDirName}/apps/`).filter(file => file.endsWith(".js"));
+//
+appFiles.forEach((file) => {
+	ret.push(import(`./apps/${file}`));
+});
+//
+ret = await Promise.allSettled(ret);
+//
+//启动插件输出日志
+let infoLog = [
+	"----------------------------",
+	`${pluginName}v${pluginVersion}插件开始初始化...`,
+];
+//打印日志
+for (let log of infoLog) logger.info(log);
+//载入apps内JS插件
+for (let item in targetFiles) {
+	let name = targetFiles[item].replace(/(\.js)$/g, "");
+	if (ret[item].status != "fulfilled") {
+		logger.error(`载入插件错误：${logger.red(name)}`);
+		logger.error(ret[i].reason);
+		continue;
+	}
+	apps[name] = ret[item].value[Object.keys(ret[item].value)[0]];
+}
+//
+logger.info(`${pluginName}插件载入成功`);
+//
+export { pluginDirName , apps };
